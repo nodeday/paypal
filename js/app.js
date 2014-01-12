cfbc6aedd5879ac5dbca9caaee7bf7137cf6d861
lmarkus <lmarkus@paypal.com>
2014-01-11 23:18:05 -0800

    Added Location

--- js/app.js
+++ js/app.js
@@ -1,23 +1,29 @@
 /**
  * Created by lmarkus on 1/10/14.
  */
-$(function(){
-   $(window).resize(function(){
-       if($( window ).width() > 640){
-           $('.nav').show();
-           $('.toggler').hide();
-       }
-       else {
-           $('.nav').hide();
-           $('.toggler').show();
-       }
-   });
+$(function () {
+    $(window).resize(function () {
+        if ($(window).width() > 640) {
+            $('.nav').removeClass('tc').show();
+            $('.toggler').hide();
+        }
+        else {
+            $('.nav').hide();
+            $('.toggler').show();
+        }
+    });
 
-    $('.toggler').click(function(){
-        $('.nav').toggle();
+    $('.toggler').click(function () {
+        var nav = $('.nav');
+        if (nav.hasClass('tc')) {
+            nav.toggle();
+        }
+        else {
+            nav.addClass('tc');
+        }
     })
 
-    $('.nav li').click(function(){
+    $('.nav li').click(function () {
         $('.nav').hide();
     });
 });
